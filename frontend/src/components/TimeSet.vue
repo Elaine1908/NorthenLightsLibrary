469560068145a9ddebf748640785c2eb5487a5b9
Sherlock_Watson <19302010069@fudan.edu.cn>
2021-05-23 18:14:07 +0800

    修改了设置时间的自动进位

--- frontend/src/components/TimeSet.vue
+++ frontend/src/components/TimeSet.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
-    <el-form :model="form" :rules="rules" ref="form">
-      <el-table :data="form.datas" highlight-current-row style="width: 100%">
+    <el-form :model="setForm" :rules="rules" ref="setForm" class="setForm">
+      <el-table :data="setForm.datas" highlight-current-row style="width: 100%">
         <el-table-column prop="role" label="身份" width="80px"></el-table-column>
 
         <el-table-column prop="max_book_borrow" label="借阅副本数" style="width:100px ">
           <template slot-scope="scope">
-            <template v-if="scope.row.action == 'view'">
+            <template v-if="scope.row.action === 'view'">
               {{scope.row.max_book_borrow}}
             </template>
             <template v-else>
-              <el-form-item :prop="'datas.'+scope.$index + '.max_book_borrow'" :rules='rules.max_book_borrow'>
-                <input class="timeInput" style="width:60px" size="mini" v-model.number="scope.row.max_book_borrow"></input>
+              <el-form-item :prop="'datas.'+scope.$index + '.max_book_borrow'" :rules='
+              {required: true, validator: (rule, value, callback) => {
+                if (value < 1 && value > 10) {
+                 callback(new Error("请输入1-10的数字"))
+              } else if (value === ""){
+              callback(new Error("请输入一个数字"))
+              } else {callback()} }, trigger: "blur"}'>
+                <input class="timeInput" style="width:60px" size="mini" v-model.number="scope.row.max_book_borrow" onkeyup = "value=value.replace(/[^\d]/g,'')"></input>
               </el-form-item>
             </template>
           </template>
@@ -23,10 +29,10 @@
               {{scope.row.max_borrow_time.day}}天{{scope.row.max_borrow_time.hour}}时{{scope.row.max_borrow_time.min}}分{{scope.row.max_borrow_time.sec}}秒
             </template>
             <template v-else>
-              <input placeholder="天" class="timeInput" v-model.number="scope.row.max_borrow_time.day"></input>天
-              <input placeholder="时" class="timeInput" v-model.number="scope.row.max_borrow_time.hour"></input>时
-              <input placeholder="分" class="timeInput" v-model.number="scope.row.max_borrow_time.min"></input>分
-              <input placeholder="秒" class="timeInput" v-model.number="scope.row.max_borrow_time.sec"></input>秒
+              <input placeholder="天" class="timeInput" v-model.number="scope.row.max_borrow_time.day" onkeyup = "value=value.replace(/[^\d]/g,'')"></input>天
+              <input placeholder="时" class="timeInput" v-model.number="scope.row.max_borrow_time.hour" onkeyup = "value=value.replace(/[^\d]/g,'')"></input>时
+              <input placeholder="分" class="timeInput" v-model.number="scope.row.max_borrow_time.min" onkeyup = "value=value.replace(/[^\d]/g,'')"></input>分
+              <input placeholder="秒" class="timeInput" v-model.number="scope.row.max_borrow_time.sec" onkeyup = "value=value.replace(/[^\d]/g,'')"></input>秒
             </template>
           </template>
         </el-table-column>
@@ -75,7 +81,7 @@
         }
       }
       return {
-        form: {
+        setForm: {
           datas: [],
         },
 
@@ -88,9 +94,9 @@
             message: '请输入最大借阅副本数',
           },
             {
-              type: 'number',
+              /*type: 'number',
               trigger: 'blur',
-              validator: validateAmount
+              validator: validateAmount*/
               // min: 1,
               // max: 10,
               // message: '最小1，最大10',
@@ -102,15 +108,15 @@
     created() {
       this.axios.get('/superadmin/userConfiguration').then(resp => {
         if (resp.status === 200) {
-          this.form.datas = resp.data.userConfigurationList;
-          for (let i = 0; i < this.form.datas.length; i++) {
-            this.form.datas[i].role = resp.data.userConfigurationList[i].role;
-            this.form.datas[i].max_book_borrow = resp.data.userConfigurationList[i].maxBookBorrow;
-            this.form.datas[i].max_borrow_time = resp.data.userConfigurationList[i].maxBorrowTime;
-            this.form.datas[i].max_reserve_time = resp.data.userConfigurationList[i].maxReserveTime;
+          this.setForm.datas = resp.data.userConfigurationList;
+          for (let i = 0; i < this.setForm.datas.length; i++) {
+            this.setForm.datas[i].role = resp.data.userConfigurationList[i].role;
+            this.setForm.datas[i].max_book_borrow = resp.data.userConfigurationList[i].maxBookBorrow;
+            this.setForm.datas[i].max_borrow_time = resp.data.userConfigurationList[i].maxBorrowTime;
+            this.setForm.datas[i].max_reserve_time = resp.data.userConfigurationList[i].maxReserveTime;
           }
           //处理数据，为已有数据添加action:'view'
-          this.form.datas.map(item => {
+          this.setForm.datas.map(item => {
             this.$set(item,"action","view")
             return item;
           });
@@ -148,7 +154,25 @@
 
       //编辑-保存操作(相当于提交按钮)
       click_save(item,index) {
-        if( !this.validateField('form',index) ) return;
+        if (item.max_borrow_time.day === undefined) {
+          item.max_borrow_time.day = 0
+        }
+        if (item.max_borrow_time.hour === undefined) {
+          item.max_borrow_time.hour = 0
+        }
+        if (item.max_borrow_time.min === undefined) {
+          item.max_borrow_time.min = 0
+        }
+        if (item.max_borrow_time.sec === undefined) {
+          item.max_borrow_time.sec = 0
+        }
+        item.max_borrow_time.min += Math.floor(item.max_borrow_time.sec / 60)
+        item.max_borrow_time.sec %= 60
+        item.max_borrow_time.hour += Math.floor(item.max_borrow_time.min / 60)
+        item.max_borrow_time.min %= 60
+        item.max_borrow_time.day += Math.floor(item.max_borrow_time.hour / 24)
+        item.max_borrow_time.hour %= 24
+        if( !this.validateField('setForm',index) ) return;
         item.action = "view";
         this.$axios.post('/superadmin/setUserConfiguration', {
           role: item.role,
@@ -176,13 +200,27 @@
 
       //编辑-取消操作
       click_cancle(item,index) {
-        this.resetField('form',index);
+        this.resetField('setForm',index);
         item.action = "view";
       },
 
       //编辑操作
       click_edit(item,index) {
         item.action = "edit";
+      }
+    },
+    mounted: () => {
+      if (!localStorage.getItem('login')) {
+        this.$message.error('请先登录')
+        this.$router.push('/login')
+      }
+      else if (localStorage.getItem('role') !== 'admin' && localStorage.getItem('role') !== 'superadmin') {
+        this.$message.error('您不是管理员，无法访问该页面')
+        this.$router.push('/home/show')
+      }
+      else if (parseInt(localStorage.getItem('exp')) < ((new Date().getTime())/1000)) {
+        this.$message.error('登录过期，请先登录')
+        this.$router.push('/login')
       }
     }
   }
