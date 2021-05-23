a27167f8d5ad4a5f518932630e7c89236a4a8ed7
张皓捷 <zhj630985214@gmail.com>
2021-05-23 21:09:15 +0800

    修正了FileNotFoundException没有继承接口NotFoundException的问题

--- backend/src/main/java/com/example/lab2/exception/notfound/FineNotFoundException.java
+++ backend/src/main/java/com/example/lab2/exception/notfound/FineNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.lab2.exception.notfound;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class FineNotFoundException extends RuntimeException{
+public class FineNotFoundException extends RuntimeException implements NotFoundException{
     public FineNotFoundException(String message) {
         super(message);
     }
