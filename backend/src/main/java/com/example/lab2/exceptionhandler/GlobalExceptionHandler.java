4463bccc9c38ebd7c6f688de61f6edd56dde1310
张皓捷 <zhj630985214@gmail.com>
2021-06-14 23:07:13 +0800

    将使全局异常处理器能接收filter和inteceptor发出的异常

--- backend/src/main/java/com/example/lab2/exceptionhandler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/lab2/exceptionhandler/GlobalExceptionHandler.java
@@ -2,14 +2,32 @@ package com.example.lab2.exceptionhandler;
 
 import com.example.lab2.controller.*;
 import com.example.lab2.exception.notfound.NotFoundException;
+import com.example.lab2.filter.JwtAuthenticationFilter;
+import com.example.lab2.filter.JwtLoginFilter;
+import com.example.lab2.filter.MakeInStreamRereadableFilter;
+import com.example.lab2.interceptor.CommentReplyInterceptor;
+import com.example.lab2.interceptor.UserCreditInterceptor;
 import com.example.lab2.response.GeneralResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice(assignableTypes = {AdminController.class, InitializeController.class,
-        ReserveController.class, SearchController.class, SuperAdminController.class, AuthController.class,NormalUserController.class})
+@ControllerAdvice(assignableTypes = {
+        AuthController.class,
+        AdminController.class,
+        BorrowController.class,
+        InitializeController.class,
+        NormalUserController.class,
+        ReserveController.class,
+        SearchController.class,
+        SuperAdminController.class,
+        UserAdminController.class,
+        CommentReplyInterceptor.class,
+        UserCreditInterceptor.class,
+        JwtAuthenticationFilter.class,
+        JwtLoginFilter.class,
+        MakeInStreamRereadableFilter.class})
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
